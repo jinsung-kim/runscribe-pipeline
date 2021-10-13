6f16120eed8643c2b634fad6be065d264bb5b446
Jin Kim <jinsungkim1210@gmail.com>
2021-10-13 09:55:09 -0400

    feat: session data being printed

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+
+get_token_url = 'https://api.runscribe.com/v2/authenticate'
+get_runs_url = 'https://api.runscribe.com/v2/runs'
+
+def main():
+    body1 = {
+        "email": "rs6418@nyu.edu",
+        "password": "WallW25?"
+    }
+
+    res = requests.post(get_token_url, data=body1)
+
+    token = res.json()["token"]
+
+    # print(token)
+
+    header = {
+        "X-Auth-Token": token
+    }
+
+    res2 = requests.get(get_runs_url, headers=header)
+
+    print(res2.json()["runs"])
+
+
+
+
+if __name__ == "__main__":
+    main()
