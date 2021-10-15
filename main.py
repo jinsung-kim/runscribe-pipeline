eec66c2cd5ebc49c542e78c6302e309c48f3c931
Jin Kim <jinsungkim1210@gmail.com>
2021-10-15 12:54:11 -0400

    feat: getting correct csv links

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ https://github.com/richardjy/read-runscribe
 
 get_token_url = 'https://api.runscribe.com/v2/authenticate'
 get_runs_url = 'https://api.runscribe.com/v2/runs'
+get_csv_url = 'https://dashboard.runscribe.com/runs/{}/mountings/{}.csv'
 
 def main():
     body1 = {
@@ -21,15 +22,31 @@ def main():
 
     # print(token)
 
+    # TODO: Check if header is required to get CSV files
     header = {
         "X-Auth-Token": token
     }
 
     res2 = requests.get(get_runs_url, headers=header)
 
-    print(res2.json()["runs"])
+    left_serial = []
+    right_serial = []
 
+    sessions = len(res2.json()["runs"])
 
+    for i in range(sessions):
+        curr_session = res2.json()["runs"][i]
+        # curr_id = curr_session["id"]
+        curr_location = curr_session["location"]
+
+        # print(curr_location)
+        # print(curr_session['run_files'])
+        left_serial.append(curr_location + "/" + curr_session['run_files'][1]['serial'] + ".csv")
+        right_serial.append(curr_location + "/" + curr_session['run_files'][0]['serial'] + ".csv")
+
+    # print(res2.json()["runs"][0])
+    print(left_serial)
+    print(right_serial)
 
 
 if __name__ == "__main__":
